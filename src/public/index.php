7cb348c6ac6fca73bdcd12400a79ec862ce5e74e
nakatsuji masato <nakatsuji@teastalk.jp>
2021-07-05 16:26:48 +0900

    7/5

--- src/public/index.php
+++ src/public/index.php
@@ -1,8 +1,26 @@
 <?php
 
+/**
+ *  Mk2 Startor
+ * 
+ *  Run the Generator of the PHP framework "Mk2".  
+ *  Check if the following required dependent packages are installed from Composer.
+ * 
+ *  - mk2\libraries
+ * 
+ */
+
 use Mk2\Libraries\Generator;
 
-require "../../vendor/autoload.php";
+$composerPath = "../../vendor/autoload.php";
+
+if(!file_exists($composerPath)){
+    // If the Composer Autoloader does not exist, display the following message and exit.
+    echo "Dependent packages not found. <br>Check if the dependent packages are installed from Composer.";
+    return;
+}
+
+require $composerPath;
 
 define("MK2_ROOT",dirname(__DIR__));
 
